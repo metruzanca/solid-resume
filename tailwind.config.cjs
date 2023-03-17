3edd89b72ddec483e259beebf157b16436a02187
Sam Zanca <sam@zanca.dev>
2023-03-17 13:58:56 -0400

    fixed responsiveness

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {},
+    // Had to change this size to make the print look like desktop
+    screens: {
+      sm: { min: "600px" },
+    }
   },
   plugins: []
 };

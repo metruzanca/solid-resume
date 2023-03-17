0ab846770621c573ec7921aeb3aba0c47d0eaf1c
Sam Zanca <sam@zanca.dev>
2023-03-17 14:26:14 -0400

    vercel adapted

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+import vercel from "solid-start-vercel"
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({
+    adapter: vercel({})
+  })],
 });
